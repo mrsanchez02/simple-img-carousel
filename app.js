8d2435ef41e34c64fdb0c5e3ae8cbed0d8d2e928
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2022-10-09 00:11:50 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".carousel").forEach(carousel =>{
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, 
+    () => `<span class="carousel__button"></span>`);
+
+  carousel.insertAdjacentHTML("beforeend", 
+  `<div class="carousel__nav">
+      ${buttonsHtml.join("")}
+    </div>
+  `);
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click",() => {
+      // Un-select al the items
+      items.forEach(item => item.classList.remove("carousel__item--selected"));
+      buttons.forEach(button=> button.classList.remove("carousel__button--selected"));
+
+      items[i].classList.add("carousel__item--selected");
+      button.classList.add("carousel__button--selected");
+    })
+  })
+
+  //Select the first item on carousel
+  items[0].classList.add("carousel__item--selected");
+  buttons[0].classList.add("carousel__button--selected");
+});
